deba41a6fa29a2a4af168415b4ee46a661d8c1da
Shrikant Vashishtha <vashishtha.sk@gmail.com>
2012-04-11 16:17:48 +0800

    fixed ClasspathToPomConverterTest

--- src/test/java/com/shri/eclipsetomaven/pom/ClasspathToPomConverterTest.java
+++ src/test/java/com/shri/eclipsetomaven/pom/ClasspathToPomConverterTest.java
@@ -4,23 +4,38 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import com.shri.eclipsetomaven.pom.ClasspathToPomConverter;
 import com.shri.eclipsetomaven.util.XMLUtil;
 
 public class ClasspathToPomConverterTest {
 	ClasspathToPomConverter converter;
 	
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+
+	private File newFolder;
+	
+	@Before
+	public void setup() throws IOException {
+		newFolder = tempFolder.newFolder();
+
+	}
+	
 	@Test
 	public void testJarNamesWithoutVersion() throws Exception {
 		Document classpathDoc = XMLUtil.getDocument(this.getClass().getResourceAsStream("/classpath-sample2.xml"));
 		assertNotNull(classpathDoc);
-		converter = new ClasspathToPomConverter(classpathDoc, null, null);
+		converter = new ClasspathToPomConverter(classpathDoc, null, newFolder);
 		Document pomDoc = converter.createPomDoc();
 		List<Element> elements = XMLUtil.getElements("dependency", pomDoc.getDocumentElement());
 		boolean elementFound = false;
